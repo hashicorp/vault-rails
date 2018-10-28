e38019b4234d248da688a08abcff3d752e71b718
Eugene Gavrilov <gavrilov.ea@gmail.com>
2018-10-29 00:17:43 +0500

    update rails version in dummy app to 5.2, run migrations in environment

--- spec/dummy/config/environments/development.rb
+++ spec/dummy/config/environments/development.rb
@@ -36,4 +36,8 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  config.after_initialize do
+    ActiveRecord::MigrationContext.new(Rails.root.join('db/migrate')).migrate
+  end
 end
