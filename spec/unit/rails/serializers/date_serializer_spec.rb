aa4a1c2461f7431f6054461f53ae7fae68e960af
Murray Steele <muz@h-lame.com>
2018-12-03 15:33:47 +0000

    Tidy up serializer specs to remove extra space

--- spec/unit/rails/serializers/date_serializer_spec.rb
+++ spec/unit/rails/serializers/date_serializer_spec.rb
@@ -2,11 +2,11 @@ require 'spec_helper'
 
 describe Vault::Rails::Serializers::DateSerializer do
   it 'encodes values to strings' do
-    expect(subject.encode(Date.new(1999, 1, 1))). to eq '1999-01-01'
+    expect(subject.encode(Date.new(1999, 1, 1))).to eq '1999-01-01'
   end
 
   it 'decodes values from strings' do
-    expect(subject.decode('1999-12-31')). to eq Date.new(1999, 12, 31)
+    expect(subject.decode('1999-12-31')).to eq Date.new(1999, 12, 31)
   end
 end
 
--- spec/unit/rails/serializers/integer_serializer_spec.rb
+++ spec/unit/rails/serializers/integer_serializer_spec.rb
@@ -2,14 +2,14 @@ require 'spec_helper'
 
 describe Vault::Rails::Serializers::IntegerSerializer do
   it 'encodes values to strings' do
-    expect(subject.encode(1)). to eq '1'
-    expect(subject.encode(42)). to eq '42'
-    expect(subject.encode(23425)). to eq '23425'
+    expect(subject.encode(1)).to eq '1'
+    expect(subject.encode(42)).to eq '42'
+    expect(subject.encode(23425)).to eq '23425'
   end
 
   it 'decodes values from strings' do
-    expect(subject.decode('1')). to eq 1
-    expect(subject.decode('42')). to eq 42
-    expect(subject.decode('23425')). to eq 23425
+    expect(subject.decode('1')).to eq 1
+    expect(subject.decode('42')).to eq 42
+    expect(subject.decode('23425')).to eq 23425
   end
 end
