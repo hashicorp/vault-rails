f9ca096f3a85ad568de21797271840effcad5c09
Ahmet <ahmetabdi@gmail.com>
2019-03-06 12:45:18 +0000

    Cleanse error messages when uniqueness validation is used

--- lib/vault/rails/vault_uniqueness_validator.rb
+++ lib/vault/rails/vault_uniqueness_validator.rb
@@ -13,11 +13,20 @@ class VaultUniquenessValidator < ActiveRecord::Validations::UniquenessValidator
     encrypted_value = record.class.encrypt_value(attribute, value)
 
     super(record, encrypted_column, encrypted_value)
+
+    cleanse_error_message(record, attribute, encrypted_column, encrypted_value)
   end
 
   private
 
   def vault_options(record, attribute)
     record.class.__vault_attributes[attribute]
+  end
+
+  def cleanse_error_message(record, attribute, encrypted_column, encrypted_value)
+    return unless record.errors.key?(encrypted_column.to_sym)
+
+    record.errors.delete(encrypted_column.to_sym)
+    record.errors.add(attribute, :taken, value: encrypted_value)
   end
 end
