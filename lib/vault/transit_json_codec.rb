37b0670aab234a874cc3bf6386f58bde8c4f3997
Ivan Dimov <ivan.dimov@fundingcircle.com>
2022-01-26 17:53:31 +0200

    Extract VaultTransitJsonCodec from fca

---
+++ lib/vault/transit_json_codec.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+module Vault
+  class TransitJsonCodec
+    def initialize(key)
+      @key = key
+    end
+
+    def encrypt(plaintext)
+      return if plaintext.blank?
+
+      secret = Vault.logical.write(
+        "transit/encrypt/#{key}",
+        plaintext: Base64.strict_encode64(Oj.dump(plaintext))
+      )
+      secret.data[:ciphertext]
+    end
+
+    def decrypt(ciphertext, field_path)
+      return if ciphertext.blank?
+
+      secret = Vault.logical.write(
+        "transit/decrypt/#{key}",
+        ciphertext: ciphertext
+      )
+
+      Oj.load(Base64.strict_decode64(secret.data[:plaintext]))
+    end
+
+    private
+
+    attr_reader :key
+  end
+end
