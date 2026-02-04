5411b5128f3b418a3056040928cc5450ec1d71d3
Chris Arcand <chris@chrisarcand.com>
2026-02-04 15:12:34 -0600

    Remove Rails 5/6 compatibility code
    Since we now require Rails 7.0+, removed all EOL version checks:
    
    1. spec/dummy/config/environments/test.rb (lines 19-25)
       Removed: if config.respond_to?(:public_file_server) check and else branch
       Justification: config.public_file_server was introduced in Rails 5.0.
       The old serve_static_files API was deprecated in Rails 5.0 and removed
       in Rails 5.1. Since we require Rails 7.0+, we can safely use
       public_file_server directly.
       Reference: https://guides.rubyonrails.org/5_0_release_notes.html
    
    2. spec/dummy/config/initializers/wrap_parameters.rb (line 11)
       Removed: if respond_to?(:wrap_parameters) check
       Justification: wrap_parameters was introduced in Rails 3.1 (August 2011).
       This check was only needed for Rails 3.0 compatibility. Since we require
       Rails 7.0+, this method is always available.
       Reference: https://api.rubyonrails.org/v7.0/classes/ActionController/ParamsWrapper.html
    
    3. spec/dummy/config/initializers/assets.rb (line 7)
       Removed: if Rails.application.config.respond_to?(:assets) check
       Justification: The assets config has been available since Rails 3.1 with
       the asset pipeline. This check was for Rails 3.0 compatibility. Even in
       Rails 7+ (where asset pipeline is optional), the config still exists.
       Reference: https://guides.rubyonrails.org/asset_pipeline.html
    
    4. lib/vault/encrypted_model.rb (lines 332-340)
       Removed: Version checks for Rails 5.1, 5.2, and 6.0 change tracking APIs
       Justification: Rails 5.1 introduced saved_change_to_attribute?. Rails 5.2
       added previous_changes_include? for deprecation transition. Rails 6.0
       standardized on previous_changes.include?(). The old attribute_changed?
       API was deprecated in Rails 5.1 and removed in Rails 6.0. Since we require
       Rails 7.0+, we can use only previous_changes.include?(attribute).
       Reference: https://api.rubyonrails.org/v7.0/classes/ActiveModel/Dirty.html
       Migration guide: https://github.com/rails/rails/blob/v6.0.0/guides/source/6_0_release_notes.md

--- spec/dummy/config/initializers/assets.rb
+++ spec/dummy/config/initializers/assets.rb
@@ -4,9 +4,7 @@
 # Be sure to restart your server when you modify this file.
 
 # Version of your assets, change this if you want to expire all your assets.
-if Rails.application.config.respond_to?(:assets)
-  Rails.application.config.assets.version = '1.0'
-end
+Rails.application.config.assets.version = '1.0'
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
